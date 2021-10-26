afd04953f017cfddfe5836ac0244ad8732e83ab0
RRRRectangle <93180615+RRRRectangle@users.noreply.github.com>
2021-10-26 14:39:09 +0800

    Add files via upload

---
+++ mode_recognization.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 8 2021
+
+@author: hejuxing
+"""
+
+#package import
+import cv2
+import numpy as np
+import os
+import time
+
+import threading
+
+import modeCNN as myNN
+
+banner =  '''\nWhat would you like to do ?
+    1- Use pretrained model for gesture recognition
+    2- Train the model (you will require image samples for training under .\training)
+    3- Exit	
+    '''
+
+def Main():
+    global guessGesture, visualize, mod, gestname, path
+
+
+        
+    #Call CNN model loading callback
+    while True:
+        ans = int(input( banner))
+        if ans == 1:
+            mod = myNN.loadCNN()
+            myNN.guessGesture(mod)
+            input("Press any key to quit")
+            break
+        elif ans == 2:
+            mod = myNN.loadCNN(True)
+            myNN.trainModel(mod)
+            input("Press any key to quit")
+            break
+        
+        else:
+            print("Get out of here!!!")
+            return 0
+
+
+
+if __name__ == "__main__":
+    
+    Main()
+
+
+
